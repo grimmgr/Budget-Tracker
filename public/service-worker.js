8ca48be077b43592ae2415b65f138227de37c6b7
grimmgr <ggrimm33@gmail.com>
2020-10-21 16:06:24 -0700

    webpack

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,78 +1,90 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/index.js",
-    "/styles.css",
-    "/manifest.webmanifest",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "/assets/css/styles.css",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
+    "/dist/app.bundle.js",
+    "/dist/manifest.json",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
   ];
 const STATIC_CACHE = "static-cache-v1";
 const DATA_CACHE = "data-cache-v1";
 
 // install
-self.addEventListener("install", (evt) => {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE).then((cache) => cache.add("/api/transaction"))
-  );
-    
+self.addEventListener("install", evt => {
   // pre cache all static assets
   evt.waitUntil(
-    caches.open(STATIC_CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(STATIC_CACHE)
+    .then(cache => cache.addAll(FILES_TO_CACHE))
+    .then(() => self.skipWaiting())
   );
-
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
-  self.skipWaiting();
 });
 
 // activate
-self.addEventListener("activate", (evt) => {
+self.addEventListener("activate", evt => {
+  const currentCaches = [STATIC_CACHE, DATA_CACHE];
   evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== STATIC_CACHE && key !== DATA_CACHE) {
-            // console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches
+      .keys()
+      .then(cacheNames => {
+        // return array of cache names that are old to delete
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 // fetch
-self.addEventListener("fetch", (evt) => {
-  if (evt.request.url.includes("transaction")) {
+self.addEventListener("fetch", evt => {
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    evt.request.method !== "GET" ||
+    !evt.request.url.startsWith(self.location.origin)
+  ) {
+    evt.respondWith(fetch(evt.request));
+    return;
+  }
+
+  // handle data GET requests for data from /api routes
+  if (evt.request.url.includes("/api/transaction")) {
+    // make network request and fallback to cache if network request fails (offline)
     evt.respondWith(
       caches.open(DATA_CACHE).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            //if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            //}
-
+            cache.put(evt.request, response.clone());
             return response;
           })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
+          .catch(() => caches.match(evt.request));
+      })
     );
-
     return;
   }
 
+  // use cache first for all other requests for performance
   evt.respondWith(
-    caches.open(STATIC_CACHE).then(cache => {
-      return cache.match(evt.request).then(response => {
-        return response || fetch(evt.request);
+    caches.match(evt.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(DATA_CACHE).then(cache => {
+        return fetch(evt.request).then(response => {
+          return cache.put(evt.request, response.clone()).then(() => {
+            return response;
+          });
+        });
       });
     })
   );
